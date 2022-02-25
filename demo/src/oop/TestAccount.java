219838e29467dc54fd01000705963865d822b60e
srikanthpragada <srikanthpragada@gmail.com>
2022-02-25 18:52:46 +0530

    25-FEB

---
+++ demo/src/oop/TestAccount.java
@@ -0,0 +1,42 @@
+package oop;
+
+class Account {
+	// Instance variables 
+	private int acno;
+	private String ahname;
+	private double balance;
+	
+    // Constructor 
+	public Account(int no, String name) {
+		acno = no;
+		ahname = name;
+	}
+	// Method 
+	public void print() {
+		System.out.println(acno);
+		System.out.println(ahname);
+		System.out.println(balance);
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+}
+
+public class TestAccount {
+
+	public static void main(String[] args) {
+		 Account a1; // object reference 
+		 
+		 a1 = new Account(1, "Joe");  // object 
+		 a1.deposit(10000);
+		 a1.deposit(5000);
+		 a1.print(); 
+	
+		 
+		 Account a2 = new Account(2, "Scott");
+		 a2.print();
+		 
+	}
+
+}
